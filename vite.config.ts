6b4e74132e542ece87b00e81481e67876d56b886
Dirosaki <diegosgomes27@gmail.com>
2024-11-01 06:34:14 -0300

    feat: add components

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import federation from '@originjs/vite-plugin-federation'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	plugins: [
+		react(),
+		federation({
+			name: 'components',
+			filename: 'remoteEntry.js',
+			exposes: {
+				'./ui/button': './src/components/ui/button.tsx',
+				'./ui/card': './src/components/ui/card.tsx',
+				'./ui/checkbox': './src/components/ui/checkbox.tsx',
+				'./ui/input': './src/components/ui/input.tsx',
+				'./ui/label': './src/components/ui/label.tsx',
+				'./ui/toaster': './src/components/ui/sonner.tsx',
+			},
+			shared: ['react', 'react-dom', 'react-router-dom', '@tanstack/react-query'],
+		}),
+	],
+	build: {
+		target: 'esnext',
+		minify: false,
+		cssCodeSplit: false,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+})
