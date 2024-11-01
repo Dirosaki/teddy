e7622eab8b9fefdcb64c91a70be509b67732bb6f
Dirosaki <diegosgomes27@gmail.com>
2024-11-01 16:34:47 -0300

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,6 @@ export default defineConfig({
 	],
 	build: {
 		target: 'esnext',
-		minify: false,
-		cssCodeSplit: false,
-		modulePreload: false,
-	},
-	server: {
-		cors: true,
 	},
 	resolve: {
 		alias: {
