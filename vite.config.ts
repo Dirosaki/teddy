ca1c08b00c35cf9c8a8fce658e2bcbfc4e9020b0
Dirosaki <diegosgomes27@gmail.com>
2024-11-01 12:47:41 -0300

    feat: add dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
 
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default defineConfig({
 	plugins: [
 		react(),
